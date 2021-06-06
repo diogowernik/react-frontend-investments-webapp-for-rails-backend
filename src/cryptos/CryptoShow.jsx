080e4b371f3028dc81c17c49688c6e6b78cf37da
Diogo Wernik <diogowernik@gmail.com>
2021-06-06 13:05:41 -0300

    added show view to crypto

---
+++ src/cryptos/CryptoShow.jsx
@@ -0,0 +1,120 @@
+import React, { Component } from 'react'
+import { Redirect } from 'react-router'
+import { Container, Row, Col, Alert } from 'reactstrap'
+
+const Api = require('./Api.js')
+
+class CryptoForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      crypto: {
+        id: this.getCryptoId(props),
+        title: '',
+      },
+      redirect: null,
+      errors: []
+    }
+
+    this.setTitle = this.setTitle.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  getCryptoId(props) {
+    try {
+      return props.match.params.id
+    } catch (error) {
+      return null
+    }
+  }
+
+  setTitle(event) {
+    let newVal = event.target.value || ''
+    this.setFieldState('title', newVal)
+  }
+
+  setFieldState(field, newVal) {
+    this.setState((prevState) => {
+      let newState = prevState
+      newState.crypto[field] = newVal
+      return newState
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+
+    let crypto = {
+      title: this.state.crypto.title,
+    }
+
+    Api.saveCrypto(crypto, this.state.crypto.id)
+      .then(response => {
+        const [error, errors] = response
+        if (error) {
+          this.setState({
+            errors: errors
+          })
+        } else {
+          this.setState({
+            redirect: '/cryptos'
+          })
+        }
+      })
+  }
+
+  componentDidMount() {
+    if (this.state.crypto.id) {
+      Api.getCrypto(this.state.crypto.id)
+        .then(response => {
+          const [error, data] = response
+          if (error) {
+            this.setState({
+              errors: data
+            })
+          } else {
+            this.setState({
+              crypto: data,
+              errors: []
+            })
+          }
+        })
+    }
+  }
+
+  render() {
+    const { redirect, crypto, errors } = this.state
+
+    if (redirect) {
+      return (
+        <Redirect to={redirect} />
+      )
+    } else {
+
+      return (
+        <Container>
+          <Row>
+            <Col>
+              <h3>{crypto.title}</h3>
+
+              {errors.length > 0 &&
+                <div>
+                  {errors.map((error, index) =>
+                    <Alert color="danger" key={index}>
+                      {error}
+                    </Alert>
+                  )}
+                </div>
+              }
+
+
+            </Col>
+          </Row>
+        </Container>
+      )
+    }
+  }
+}
+
+export default CryptoForm
