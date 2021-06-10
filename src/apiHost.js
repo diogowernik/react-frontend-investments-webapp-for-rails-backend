86591b5d892539451817d0af99a0a3346c769178
Diogo Wernik <diogowernik@gmail.com>
2021-06-09 21:45:02 -0300

    contantes

---
+++ src/apiHost.js
@@ -0,0 +1,13 @@
+//const apiHost = 'http://localhost:3000'
+//const apiHost = 'https://api.miz.finance'
+
+class apiHost extends Component {
+    apiHost = 'http://localhost:3000'
+  
+    render() 
+
+            apiHost  
+    
+  }
+  
+  export default apiHost
