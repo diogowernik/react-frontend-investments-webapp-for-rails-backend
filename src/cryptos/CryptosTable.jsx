00e663f453c086ea62f63caaba828405338b7a9d
Diogo Wernik <diogowernik@gmail.com>
2021-06-29 22:35:08 -0300

    mexendo para formatar

--- src/cryptos/CryptosTable.jsx
+++ src/cryptos/CryptosTable.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import $ from 'jquery';
 import Datatable from '../Datatable.js';
 import { FaPencilAlt,FaTrashAlt  } from 'react-icons/fa';
 
@@ -10,53 +9,13 @@ class CryptosTable extends Component {
       cryptos: props.cryptos
     }
   }
-  state = {
-    dtOptions2: {
-        'paging': true, // Table pagination
-        'ordering': true, // Column ordering
-        'info': true, // Bottom left status text
-        responsive: true,
-        // Text translation options
-        // Note the required keywords between underscores (e.g _MENU_)
-        oLanguage: {
-            sSearch: '<em class="fa fa-search"></em>',
-            sLengthMenu: '_MENU_ records per page',
-            info: 'Showing page _PAGE_ of _PAGES_',
-            zeroRecords: 'Nothing found - sorry',
-            infoEmpty: 'No records available',
-            infoFiltered: '(filtered from _MAX_ total records)',
-            oPaginate: {
-                sNext: '<em class="fa fa-caret-right"></em>',
-                sPrevious: '<em class="fa fa-caret-left"></em>'
-            }
-        },
-        // Datatable Buttons setup
-        dom: 'Bfrtip',
-        buttons: [
-            { extend: 'copy', className: 'btn-info' },
-            { extend: 'csv', className: 'btn-info' },
-            { extend: 'excel', className: 'btn-info', title: 'XLS-File' },
-            { extend: 'pdf', className: 'btn-info', title: $('title').text() },
-            { extend: 'print', className: 'btn-info' }
-        ]
-    }
-  }
-  dtInstance = dtInstance => {
-      const inputSearchClass = 'datatable_input_col_search';
-      const columnInputs = $('tfoot .' + inputSearchClass);
-      // On input keyup trigger filtering
-      columnInputs
-          .keyup(function() {
-              dtInstance.fnFilter(this.value, columnInputs.index(this));
-          });
-  }
   render() {
     const cryptos = this.state.cryptos
     if (cryptos.length === 0) {
       return <div></div>
     } else {
       return (
-        <Datatable options={this.state.dtOptions2}>
+        <Datatable>
           <table className="table table-striped my-4 w-100">
             <thead >
               <tr>
